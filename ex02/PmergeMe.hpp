8d7ac553814cda80b68a2d9e58b855e1be95a48a
lle-tuto <lena.tutour@gmail.com>
2025-12-27 11:27:44 +0100

    ca commence

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -13,6 +13,26 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+#include <iostream>
+#include <cstring>
+#include <vector> 
+#include <deque>
 
+
+class PmergeMe
+{
+    private :
+    vector<int> jacob;
+    vector<int> main;
+    vector<int> pend;
+
+    public :
+    PmergeMe();
+    ~PmergeMe();
+	PmergeMe &operator=(PmergeMe const &rhs);
+    PmergeMe(PmergeMe const &src);
+
+    void jacobstahl();
+}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,3 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+void checkingSequence(std::string &sequence)
+{
+    while (i < sequence.size())
+    {
+        if (!isdigit(sequence[i]) && !isspace(sequence[i]))
+            throw std::runtime_error("wrong sequence bro");
+        //checker que seulement positif, checker que pas int max
+        //checker que max 3000 integers
+        i++;
+    }
+
+}
+
+
+int main( int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "This program takes one integer sequence" << std::endl;
+        return -1
+    }
+    try 
+    {
+        std::string sequence  = argv[1];
+        checkingSequence(sequence);
+        // sorting_pairs(argv[1]);
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "Error : " << e.what() <<  std::endl;
+    }
+}
